6c16a9189965b126c9f4a646e342a1a7f6d8d9e8
raerare <rodmeng123@gmail.com>
2026-05-11 15:36:04 +0000

    Add Week 05 Python practice scripts

---
+++ week-05/Ex3B_sample_collections/address_entry.py
@@ -0,0 +1,24 @@
+contact_info = {
+    'name': 'Jordan Lee',
+    'address': '123 Main Street',
+    'city': 'Chicago',
+    'state': 'IL',
+    'zip': '60601'
+}
+
+print(f"""{contact_info['name']}
+{contact_info['address']}
+{contact_info['city']}, {contact_info['state']} {contact_info['zip']}""")
+
+contact_info.pop('name')
+
+full_name = {
+    'first name': 'Jordan',
+    'last name': 'Lee'
+}
+full_name.update({'honorific': 'Mx.'})
+contact_info.update({'full_name': full_name})
+
+print(f"""{contact_info['full_name']['honorific']} {contact_info['full_name']['first name']} {contact_info['full_name']['last name']}
+{contact_info['address']}
+{contact_info['city']}, {contact_info['state']} {contact_info['zip']}""")
